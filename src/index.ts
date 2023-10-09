aef774846171c33833b3d02f321fc35a920ad940
Nicolas KEITA <nicolas.keita@epitech.eu>
2023-10-09 20:06:06 +0200

    added mars coordinates

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,24 @@
     Path + Filename: src/index.js.ts
 */
 
-console.log('helow world')
+class Point {
+    x: number
+    y: number
+    constructor(x: number, y: number) {
+        this.x = x
+        this.y = y
+    }
+}
+
+const mars = [
+    new Point(0, 100),
+    new Point(1000, 500),
+    new Point(1500, 1500),
+    new Point(3000, 1000),
+    new Point(4000, 150),
+    new Point(5500, 150),
+    new Point(6999, 800)
+];
+
+
+console.log('helow world2')
