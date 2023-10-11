ca51bc99bb2cb5da4da0ca6e6dc5656022fb47fe
Nicolas KEITA <nicolas.keita@epitech.eu>
2023-10-11 20:11:43 +0200

    added the rocket pos

--- __main__.py
+++ __main__.py
@@ -2,26 +2,39 @@ import sys
 import matplotlib.pyplot as plt
 from typing import List
 from src.Point2D import Point2D
+import time
 
 
 def parse_mars_surface() -> List[Point2D]:
     return [Point2D(int(x), int(y)) for x, y in (input().split(' ') for _ in range(int(input())))]
 
 
-def display_points_graph(points: List[Point2D]):
+def display_points_graph(points: List[Point2D], isolated_point: Point2D, title: str):
     x_coords, y_coords = zip(*[(point.x, point.y) for point in points])
 
-    plt.plot(x_coords, y_coords, marker='o')
+    plt.plot(x_coords, y_coords, marker='o', label='Mars Surface')
+    plt.scatter(isolated_point.x, isolated_point.y, color='red', label='Rocket')
     plt.xlabel('X Coordinate')
     plt.ylabel('Y Coordinate')
     plt.xlim(0, 7000)
     plt.ylim(0, 3000)
-    plt.title('Landing on Mars')
+    plt.title(title)
+    # plt.legend()
     plt.grid(True)
     plt.show()
 
 
 if __name__ == '__main__':
+    turn = 0
+    rocket = Point2D(2500, 2700)
     mars = parse_mars_surface()
-    display_points_graph(mars)
+    while True:
+        turn += 1
+        rocket.x -= 50
+        rocket.y -= 50
+        time.sleep(1)
+        if rocket.y <= 0:
+            break
+        print(turn)
+        display_points_graph(mars, rocket, 'Landing on Mars')
     print(mars, file=sys.stderr)
--- src/Point2D.py
+++ src/Point2D.py
@@ -1,13 +1,13 @@
 class Point2D:
-    def __init__(self, x, y):
+    def __init__(self, x: int, y: int) -> None:
         self.x = x
         self.y = y
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"Point2D({self.x}, {self.y})"
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"Point2D({self.x}, {self.y})"
 
-    def distance_to(self, other_point):
+    def distance_to(self, other_point: 'Point2D') -> float:
         return ((self.x - other_point.x) ** 2 + (self.y - other_point.y) ** 2) ** 0.5
