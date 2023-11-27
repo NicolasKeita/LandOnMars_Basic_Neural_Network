ca0c43ada1386b2808001874db8ca30ce6c79ffe
Nicolas KEITA <nicolas.keita@epitech.eu>
2023-11-27 08:40:53 +0100

    update requirements

--- src/graph_handler.py
+++ src/graph_handler.py
@@ -18,7 +18,7 @@ def plot_mean_rewards(rewards, ax):
     ax.set_xlabel('Iteration')
     ax.set_ylabel('Average Reward')
     ax.set_title('Training Progress - mean rewards during entire horizon')
-    plt.pause(0.0001)
+    plt.pause(0.001)
 
 
 def plot_terminal_state_rewards(rewards, ax):
@@ -26,14 +26,12 @@ def plot_terminal_state_rewards(rewards, ax):
     ax.set_xlabel('Iteration')
     ax.set_ylabel('Average Reward')
     ax.set_title('Training Progress - terminal state reward')
-    plt.pause(0.0001)
+    plt.pause(0.001)
 
 
 def display_graph(trajectories: list[list[tuple[float, float]]], id_lines_color: int, ax):
-
     cmap = cm.get_cmap('Set1')
     color = cmap(id_lines_color % 9)
-    # ax = plt.gca()
 
     # Clear previous trajectories
     for line in ax.lines:
@@ -44,14 +42,5 @@ def display_graph(trajectories: list[list[tuple[float, float]]], id_lines_color:
         x_values = [point[0] for point in trajectory]
         y_values = [point[1] for point in trajectory]
         ax.plot(x_values, y_values, marker='o',
-                 markersize=2, color=color, label=f'Rocket {id_lines_color}')
-        plt.pause(0.0001)
-
-
-def display_grid(grid):
-    # Convert the boolean values to integers (0 for False, 1 for True)
-    array_data = np.array(grid, dtype=int)
-
-    # Plot the binary image
-    plt.imshow(array_data, cmap='binary', interpolation='none', origin='lower')
-    plt.show()
+                markersize=2, color=color, label=f'Rocket {id_lines_color}')
+        plt.pause(0.001)
