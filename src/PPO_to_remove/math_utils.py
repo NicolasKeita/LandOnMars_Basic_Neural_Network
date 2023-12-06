e447e2aa051642d5fd30313e843aa7eafa877ff1
Nicolas Keita <nicolaskeita2@gmail.com>
2023-12-06 20:07:46 +0100

    pivot DDPG + HER

--- src/PPO_to_remove/math_utils.py
+++ src/PPO_to_remove/math_utils.py
@@ -11,3 +11,21 @@ def distance_squared_to_line(point, line_segments: LineString) -> float:
     point = Point(point)
     projected_point = line_segments.interpolate(line_segments.project(point))
     return distance_squared(projected_point, point)
+
+# def distance_squared_to_line(point, line_segments):
+#     x, y = point
+#     min_distance_squared = float('inf')
+#
+#     for segment in line_segments:
+#         (x1, y1), (x2, y2) = segment
+#         dx, dy = x2 - x1, y2 - y1
+#
+#         # Calculate the squared distance
+#         dot_product = (x - x1) * dx + (y - y1) * dy
+#         t = max(0, min(1, dot_product / (dx ** 2 + dy ** 2)))
+#         closest_point = (x1 + t * dx, y1 + t * dy)
+#
+#         segment_distance_squared = distance_squared((x, y), closest_point)
+#         min_distance_squared = min(min_distance_squared, segment_distance_squared)
+#
+#     return min_distance_squared
