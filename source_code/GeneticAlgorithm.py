456860e16f2e72f4d8e681a64c10d09870384af2
Nicolas KEITA <nicolas.keita@epitech.eu>
2024-02-04 10:09:26 +0100

    refacto cleaning

--- source_code/GeneticAlgorithm.py
+++ source_code/GeneticAlgorithm.py
@@ -151,7 +151,7 @@ class GeneticAlgorithm:
             action_to_do[1] = 0
         return action_to_do
 
-    def selection(self, rewards: np.ndarray):
+    def selection(self, rewards: ArrayLike):
         sorted_indices = np.argsort(rewards)
         parents = self.population[sorted_indices[-self.n_elites:]]
         return parents
